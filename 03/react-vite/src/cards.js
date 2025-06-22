6756ac9f17f248dc75bf5f853b99f63943793064
it-human <it@humancta.org>
2025-06-22 13:49:55 +0200

    03 - card component

---
+++ 03/react-vite/src/cards.js
@@ -0,0 +1,37 @@
+export const cards = [
+  {
+    "name": "Goku",
+    "image": "https://dragonball-api.com/characters/goku_normal.webp",
+    "attack": 100,
+    "defens": 90,
+    "ki": 1000
+  },
+  {
+    "name": "Vegeta",
+    "image": "https://dragonball-api.com/characters/vegeta_normal.webp",
+    "attack": 95,
+    "defens": 88,
+    "ki": 900
+  },
+  {
+    "name": "Piccolo",
+    "image": "https://dragonball-api.com/characters/picolo_normal.webp",
+    "attack": 70,
+    "defens": 80,
+    "ki": 700
+  },
+  {
+    "name": "Freezer",
+    "image": "https://dragonball-api.com/characters/Freezer.webp",
+    "attack": 98,
+    "defens": 90,
+    "ki": 950
+  },
+  {
+    "name": "Cell",
+    "image": "https://dragonball-api.com/characters/celula.webp",
+    "attack": 99,
+    "defens": 100,
+    "ki": 999
+  }
+]
